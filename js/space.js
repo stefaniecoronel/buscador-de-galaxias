d0ed70a5f404f7b6ff550525f743239ca98287f8
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-10-07 11:55:11 -0300

    Aparecen cards, faltan imagenes

--- js/space.js
+++ js/space.js
@@ -4,20 +4,55 @@ document.getElementById('btnBuscar').addEventListener('click', function(){
         let galaxiesJSON = `https://images-api.nasa.gov/search?q=${searchInput}`
         getJSONData(galaxiesJSON).then(function(respObj){
             if(respObj.status=="ok"){
-                galaxiesArray=respObj.data;
+                galaxiesArray=respObj.data.collection.items;
                 console.log(galaxiesArray);
+                showGalaxies(galaxiesArray);
             }
         });
 
     }
     else {
-        alert("Debe ingresar parámetro de búesqueda.")
+        alert("Debe ingresar parámetro de búsqueda.")
     }
+
     
 }); 
 
+let galaxiesContainer = document.getElementById('contenedor')
+
+function showGalaxies (array) {
+let imgJSONArray = [];
+array.forEach(element => { 
+    let imgJSON = element.href
+    imgJSONArray.push(imgJSON)
+});
+let galaxiesImgArray = [];
+imgJSONArray.forEach(element => {
+    getJSONData(element).then(function(respObj){
+        if (respObj.status=="ok"){
+            galaxiesImgArray.push(respObj.data)
+        }
+    })
+    
+});
 
 
+galaxiesContainer.innerHTML =""
+
+array.forEach(element => {
+    galaxiesContainer.innerHTML += `
+    <div class="card">
+        
+        <div class="card-body">
+            <h5 class="card-title">${element.data[0].title}</h5>
+            <p class="card-text">${element.data[0].description}</p>
+            <small>${element.data[0].date_created}</small>
+        </div>
+    </div>
+    `
+});
+
+};
 
 
 
